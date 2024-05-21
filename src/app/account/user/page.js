3ca367b7b82b94444c9fe214b54842c30f2cc2fc
JSFlobert170 <flobertjohnson@gmail.com>
2024-05-21 20:06:58 +0200

    header with profil and bourse button

--- src/app/account/user/page.js
+++ src/app/account/user/page.js
@@ -1,16 +1,12 @@
 "use client";
 import TitlePage from "@/components/UI/TitlePage";
 import React, { useState, useEffect } from "react";
-import UserTable from "@/components/UI/Table";
-import useFetch from "@/hooks/useFetch";
 import { getUser } from "@/services/api/user.api";
 import { CookiesProvider, useCookies } from 'react-cookie'
 import jwt from "jsonwebtoken";
 
 import Link from "next/link";
 import Button from "@/components/UI/Button";
-import { useParams } from "next/navigation";
-import Input from "@/components/UI/Input";
 
 const UserBackOffice = () => {
     const [usersList, setUsersList] = useState(null);
@@ -68,7 +64,7 @@ const UserBackOffice = () => {
                                         className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
                                     />
                                 </Link>
-                                <Link href={`/account/users/${user._id}`}>
+                                <Link href={`/account/user/${user._id}`}>
                                     <Button
                                         className="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
                                         title="Modifier"
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 "use client";
 // import Header from "@/components/partials/Header";
 import HeaderBackoffice from "@/components/partials/HeaderBackOffice";
-// import Footer from "@/components/partials/Footer";
+import Footer from "@/components/partials/Footer";
 import "@/assets/styles/style.scss";
 import { usePathname } from "next/navigation";
 import { DM_Serif_Display, Work_Sans } from "next/font/google";
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
 
     return (
         <html lang="en">
-            {pathname.includes("/backoffice") ? (
+            {pathname.includes("/account") ? (
                 <body
                     className={`${dm_serif_display.className} ${work_sans.className} flex`}
                 >
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
                 >
                     {/* <Header /> */}
                     <main>{children}</main>
-                    {/* <Footer /> */}
+                    <Footer />
                 </body>
             )}
         </html>
